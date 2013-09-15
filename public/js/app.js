a1cf0a2f9a873df69461f48a69fea7dd78829318
Tom Shires <tom@doubletaps.net>
2013-09-15 15:08:50 -0700

    add some stuff to home page

--- public/js/app.js
+++ public/js/app.js
@@ -5,12 +5,12 @@
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
-    	when('/',				{templateUrl: 'partials/index',					controller: IndexCtrl}).
-  		when('/listUsers',		{templateUrl: 'partials/listUsers',		controller: ListUsersCtrl}). 
-    	when('/addUser', 		{templateUrl: '/partials/addUser',		controller: AddUserCtrl}).
-    	when('/editUser/:id',	{templateUrl: '/partials/editUser',		controller: EditUserCtrl}).
-    	when('/showUser/:id',	{templateUrl: '/partials/showUser',		controller: ShowUserCtrl}).
-    	when('/deleteUser/:id',	{templateUrl: '/partials/deleteUser',	controller: DeleteUserCtrl}).
+    	when('/',					{templateUrl: 'partials/index',				controller: IndexCtrl}).
+  		when('/listUsers',			{templateUrl: 'partials/listUsers',			controller: ListUsersCtrl}). 
+    	when('/addUser', 			{templateUrl: '/partials/addUser',			controller: AddUserCtrl}).
+    	when('/editUser/:id',		{templateUrl: '/partials/editUser',			controller: EditUserCtrl}).
+    	when('/showUser/:id',		{templateUrl: '/partials/showUser',			controller: ShowUserCtrl}).
+    	when('/deleteUser/:id',		{templateUrl: '/partials/deleteUser',		controller: DeleteUserCtrl}).
 
 
   		when('/listLocations',		{templateUrl: 'partials/listLocations',		controller: ListLocationsCtrl}). 
@@ -19,11 +19,11 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives'])
     	when('/showLocation/:id',	{templateUrl: '/partials/showLocation',		controller: ShowLocationCtrl}).
     	when('/deleteLocation/:id',	{templateUrl: '/partials/deleteLocation',	controller: DeleteLocationCtrl}).
 
-  		when('/listMovies',		{templateUrl: 'partials/listMovies',		controller: ListMoviesCtrl}). 
-    	when('/addMovie', 		{templateUrl: '/partials/addMovie',		controller: AddMovieCtrl}).
-    	when('/editMovie/:id',	{templateUrl: '/partials/editMovie',		controller: EditMovieCtrl}).
-    	when('/showMovie/:id',	{templateUrl: '/partials/showMovie',		controller: ShowMovieCtrl}).
-    	when('/deleteMovie/:id',	{templateUrl: '/partials/deleteMovie',	controller: DeleteMovieCtrl}).
+  		when('/listMovies',			{templateUrl: 'partials/listMovies',		controller: ListMoviesCtrl}). 
+    	when('/addMovie', 			{templateUrl: '/partials/addMovie',			controller: AddMovieCtrl}).
+    	when('/editMovie/:id',		{templateUrl: '/partials/editMovie',		controller: EditMovieCtrl}).
+    	when('/showMovie/:id',	{	templateUrl: '/partials/showMovie',			controller: ShowMovieCtrl}).
+    	when('/deleteMovie/:id',	{templateUrl: '/partials/deleteMovie',		controller: DeleteMovieCtrl}).
 
 
     	otherwise({redirectTo: '/'});
